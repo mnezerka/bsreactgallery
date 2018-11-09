a8f84156cf202e2bdd3d3fc3d6cf542b6c7eed82
michaln <michal.nezerka@gmail.com>
2018-11-09 21:16:38 +0100

    Support for multiple galleries

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# BSReactGallery
+
+Simple gallery indended for integration into WordPress plugin
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Gallery from 'react-grid-gallery';
 
-ReactDOM.render(
-    <Gallery
-        enableImageSelection={false}
-        images={window.BSGALLERYIMAGES}
-    />,
-    document.getElementById(window.BSGALLERYNODEID)
-);
+if (window.BSPOSTGALLERY)
+{
+    for (let index = 0; index < window.BSPOSTGALLERY.length; ++index)
+    {
+        let gallery = window.BSPOSTGALLERY[index];
+
+        ReactDOM.render(
+            <Gallery
+                enableImageSelection={false}
+                images={gallery.images}
+            />,
+            document.getElementById(gallery.node)
+        );
+    }
+}
